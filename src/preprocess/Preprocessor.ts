91880f15798cbc1fb479e8e02c3970ac9742b5fe
Robert Albus <hello@robertalbus.com>
2020-12-30 17:32:32 -0700

    Implement Preprocessor. It ingests the pizza order string and converts it into an Order object.

--- src/preprocess/Preprocessor.ts
+++ src/preprocess/Preprocessor.ts
@@ -1,13 +1,53 @@
 import { InputData, IPipelineStage, PreprocessedData } from '../pipeline';
-import { Order } from '../pipeline/dto-types';
+import { Order, Pizza } from '../pipeline/dto-types';
 
 export interface IPreprocessor extends IPipelineStage<InputData, PreprocessedData> {}
 
 export class Preprocessor implements IPreprocessor, IPipelineStage<InputData, PreprocessedData> {
+  private data = {} as Order;
+
   ingest(input?: string): void {
-    throw new Error('Method not implemented.');
+    if (!input || typeof input != 'string') {
+      throw new Error('Preprocessor received invalid input.');
+    }
+
+    this.data = this.process(input);
   }
+
   getResult(): Order {
-    throw new Error('Method not implemented.');
+    return this.data;
+  }
+
+  private process(input: string): Order {
+    const order: Order = {
+      pizzas: [],
+    };
+
+    //split to individual pizzas
+    const pizzas = input.split('\n');
+
+    // split pizza string into pizza objects
+    pizzas.forEach((pizzaString) => {
+      const pizza = {} as Pizza;
+      pizza.size = this.getSizeFromPizzaString(pizzaString);
+      pizza.toppings = this.getToppingsFromPizza(pizzaString) || [];
+      order.pizzas.push(pizza);
+    });
+
+    return order;
+  }
+
+  getToppingsFromPizza(pizza: string): string[] {
+    return pizza
+      .replace(/\s/g, '')
+      .split('-')[1]
+      .split(',')
+      .filter((topping) => {
+        return !!topping;
+      });
+  }
+
+  private getSizeFromPizzaString(pizza: string): string {
+    return pizza.replace(/\s/g, '').split('-')[0];
   }
 }
