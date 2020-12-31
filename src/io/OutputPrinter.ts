93ed0196d33374363b5e6d4b9b3e62fbb9b875de
Robert Albus <hello@robertalbus.com>
2020-12-30 21:15:54 -0700

    Implement OutputPrinter module

--- src/io/OutputPrinter.ts
+++ src/io/OutputPrinter.ts
@@ -1,13 +1,53 @@
 import { CalculatedResult, IPipelineStage } from '../pipeline';
 import { OrderWithPrice } from '../pipeline/dto-types';
+import { default as equal } from 'deep-equal';
 
 export interface IOutput extends IPipelineStage<CalculatedResult, void> {}
 
 export class OutputPrinter implements IOutput, IPipelineStage<CalculatedResult, void> {
+  private data: string = '';
+
   ingest(input?: OrderWithPrice): void {
-    throw new Error('Method not implemented.');
+    if (!input) {
+      throw new Error('Output Printer did not receive data');
+    }
+    this.data = this.process(input);
   }
+
   getResult(): void {
-    throw new Error('Method not implemented.');
+    console.clear();
+    console.log(this.data);
+  }
+
+  private process(order: OrderWithPrice): string {
+    let outputString = '';
+
+    while (order.pizzas.length) {
+      const pizza = order.pizzas.shift();
+      if (!pizza) break;
+
+      // count matched pizzas
+      let count = 1;
+      const pizzaMatches = order.pizzas.filter((comparePizza) => {
+        return equal(pizza, comparePizza);
+      });
+      count += pizzaMatches.length;
+
+      // remove matched pizzas from list
+      order.pizzas = order.pizzas.filter((comparePizza) => {
+        return !equal(pizza, comparePizza);
+      });
+
+      // build output for pizza items
+      outputString += `${count} ${pizza.size}, ${pizza.toppings.length} topping pizza - `;
+      outputString += `${pizza.toppings.join(', ')}: ${pizza.price} \n`;
+    }
+
+    // build output for order price
+    outputString += `Subtotal: \$ ${order.subtotal.toFixed(2)} \n`;
+    outputString += `GST: \$ ${order.tax.toFixed(2)} \n`;
+    outputString += `Total: \$ ${order.total.toFixed(2)} \n`;
+
+    return outputString;
   }
 }
